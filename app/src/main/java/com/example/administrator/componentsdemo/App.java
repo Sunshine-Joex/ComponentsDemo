5838d55f0cda22c1f4bf4b1fbd5cf808eb7fd61f
QLY <1532888097@qq.com>
2018-05-25 15:23:10 +0800

    join ARouter library\

--- app/src/main/java/com/example/administrator/componentsdemo/App.java
+++ app/src/main/java/com/example/administrator/componentsdemo/App.java
@@ -1,10 +1,16 @@
 package com.example.administrator.componentsdemo;
 
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.common_module.BaseApplication;
 
 public class App extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        if(BuildConfig.DEBUG){
+            ARouter.openLog();
+            ARouter.openDebug();
+        }
+        ARouter.init(this);
     }
 }
--- main_module/src/main/java/com/example/main_module/HomeActivity.java
+++ main_module/src/main/java/com/example/main_module/HomeActivity.java
@@ -3,6 +3,9 @@ package com.example.main_module;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+@Route(path = "/main/HomeActivity")
 public class HomeActivity extends AppCompatActivity {
 
     @Override
